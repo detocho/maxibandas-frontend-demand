454a0fc17e95c2f3aaca02fff02db396aacfda8c
davidpazgtz764 <dpaz@Air-de-David.lan>
2015-04-17 12:43:29 -0500

    Quitamos la BD de produccion

--- webserver/grails-app/conf/DataSource.groovy
+++ webserver/grails-app/conf/DataSource.groovy
@@ -13,8 +13,7 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+
         }
     }
     test {
@@ -25,19 +24,7 @@ environments {
     }
     production {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            pooled = true
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
-            }
+
         }
     }
 }
